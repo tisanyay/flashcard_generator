92cbf2178ed4695449df0951b05db1e555404408
yisan <brendentay@gmail.com>
2023-04-15 20:33:00 +0800

    connected openai api to app and updated README.md

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # flashcard_generator
-### 1. Install dependencies
+### 1. Change to root directory of project
+```sh
+$ cd $root_directory
+```
+
+### 1. Setup virtual environment and activate environment
+```sh
+$ python3 -m venv env
+```
+#### Linux
+```sh
+$ source env/bin/activate
+```
+
+#### Windows
+```sh
+$ env/Scripts/activate
+```
+
+### 2. Install dependencies
 ```sh
 $ pip install -r requirements.txt
 ```
 
-### 2. Run streamlit app
+### 3. Run streamlit app
 ```sh
 $ streamlit run app.py
 ```
--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import requests
+import asyncio
 from streamlit_lottie import st_lottie
 from ai_api.doc_ocr import extract_text
 from ai_api.tokenize_text import chunkify
 from ai_api.chadgpt import generate_qa
+from streamlit_card import card
+
+def generate_flashcards(bytes_data):
+    document_object = extract_text(bytes_data)
+    chunks = chunkify(document_object.text)
+    qa_pairs = generate_qa(chunks)
+    return qa_pairs
 
 def load_lottieurl(url):
     r = requests.get(url)
@@ -13,6 +21,7 @@ def load_lottieurl(url):
 
 # --- animation asset ---
 flashcard_animation = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_jxgqawba.json")
+spinning_animation = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_b88nh30c.json")
 
 # --- headers ---
 st.set_page_config(page_title="Flashcard Generator", page_icon=":card_index:", layout="wide")
@@ -26,7 +35,7 @@ with st.container():
         st.title("Automate flashcard creation by uploading any PDF file")
         st.write("Flashcards are excellent as they exercise active recall when used effectively. However, creating the \
                 flashcards are a hassle and is one of the main reasons people don't use them more often. This app \
-                aims to solve this issue and make flashcards a viable option for every student!")
+                aims to solve this issue and make flashcards a viable option for every learner!")
     with right_column:
         st_lottie(flashcard_animation, height=300, key="flashcards")
 
@@ -34,28 +43,35 @@ with st.container():
 st.write('---')
 uploaded_file = st.file_uploader("Choose a PDF file")
 
+
+# cols = st.columns(4)
+
+# --- test cards ---
+# with cols[1]:
+    # card(
+      # title="Hello World!",
+      # text="Some description",
+      # image="http://placekitten.com/200/300",
+      # url="https://github.com/gamcoh/st-card"
+    # )
+
+# with cols[2]:
+    # card(title='answer', text='')
+
+
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
 
-    document_object = extract_text(bytes_data)
-    
-    chunks = chunkify(document_object.text)
+    # loading_animation = st_lottie(spinning_animation, height=100, key="Please wait...")
+    with st.spinner('Generating cards...'):
+        qa_pairs = generate_flashcards(bytes_data)
 
-    qa_pairs = generate_qa(chunks)
+    cols = st.columns(4)
 
     for q, a in qa_pairs:
-        st.write(q)
-        st.write(a)
+        with cols[1]:
+            card(title='', text=q)
+        with cols[2]:
+            card(title='', text=a)
 
-    # To convert to a string based IO:
-    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    # st.write(stringio)
-
-    # To read file as string:
-    # string_data = stringio.read()
-    # st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    # dataframe = pd.read_csv(uploaded_file)
-    # st.write(dataframe)
