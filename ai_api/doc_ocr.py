9f762d08cd3155cddaf2ac0b3578bac0f454b56d
yisan <brendentay@gmail.com>
2023-04-15 13:21:03 +0800

    give up on workflow identity federation

--- ai_api/doc_ocr.py
+++ ai_api/doc_ocr.py
@@ -20,10 +20,6 @@ def extract_text(image_content):
 
     RESOURCE_NAME = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
 
-    # Read the file into memory
-    # with open(FILE_PATH, "rb") as image:
-        # image_content = image.read()
-
     # Load Binary Data into Document AI RawDocument Object
     raw_document = documentai.RawDocument(content=image_content, mime_type=MIME_TYPE)
 
@@ -38,30 +34,11 @@ def extract_text(image_content):
 if __name__ == "__main__":
     FILE_PATH = './test_pdf/pythagoras.pdf'
 
-    # Instantiates a client
-    docai_client = documentai.DocumentProcessorServiceClient(
-        client_options=ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com")
-    )
-
-    # The full resource name of the processor, e.g.:
-    # projects/project-id/locations/location/processor/processor-id
-    # You must create new processors in the Cloud Console first
-    RESOURCE_NAME = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
-
     # Read the file into memory
     with open(FILE_PATH, "rb") as image:
         image_content = image.read()
 
-    # Load Binary Data into Document AI RawDocument Object
-    raw_document = documentai.RawDocument(content=image_content, mime_type=MIME_TYPE)
-
-    # Configure the process request
-    request = documentai.ProcessRequest(name=RESOURCE_NAME, raw_document=raw_document)
-
-    # Use the Document AI client to process the sample form
-    result = docai_client.process_document(request=request)
-
-    document_object = result.document
+    document_object = extract_text(image_content)
 
     print("Document processing complete.")
     # print(f"Text: {document_object.text}")
--- test.py
+++
@@ -1,7 +0,0 @@
-gcloud iam service-accounts add-iam-policy-binding "githubsecret@flashcard-generator-383608.iam.gserviceaccount.com" \
-  --project="flashcard-generator-383608" \
-  --role="roles/iam.workloadIdentityUser" \
-  --member="principalSet://iam.googleapis.com/projects/189439450845/locations/global/workloadIdentityPools/flashcardgeneratorkeys/attribute.repository/${REPO}"
-
-principalSet://iam.googleapis.com/projects/189439450845/locations/global/workloadIdentityPools/flashcardgeneratorkeys/*
-
